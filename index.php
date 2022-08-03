63f9aa68bdf755ff91d5e77762292563f71c7eb6
Felipe S. Anjos <felipe@serpadosanjos.com>
2022-08-03 20:59:18 -0300

    ''

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="DESCRIPTION" CONTENT="" />
+        <meta name="KEYWORDS" CONTENT="" />
+        <meta name="RATING" CONTENT="general" />
+        <meta name="DISTRIBUTION" CONTENT="global" />
+        <meta name="LANGUAGE" CONTENT="pt-BR" />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="author" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="" />
+        <meta property="og:title" content="" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+        <meta property="og:image:alt" content="" />
+        <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="200" /> 
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:title" content="">
+        <meta property="twitter:description" content="">
+        <meta property="twitter:image" content="">
+        <title>HTML + CSS + BOOTSTRAP + JS (BASE STARTER)</title>
+        <link rel="shortcut icon" href="" type="image/x-icon" />
+        <link rel="stylesheet" href="./assets/css/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="./assets/fonts/fontawesome-free-6.1.2-web/css/all.min.css" />
+        <link rel="stylesheet" href="./assets/css/style.css" />
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+        
+        <script src="./assets/plugin/js/jquery-3.6.0.min.js"></script>
+        <script src="./assets/plugin/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
